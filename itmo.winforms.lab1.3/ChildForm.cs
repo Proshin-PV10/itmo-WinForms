f92feec4b6786a1edd35c2b29ad7c782ff2f37d3
Proshin-PV10 <167138897+Proshin-PV10@users.noreply.github.com>
2024-07-23 22:07:17 +0300

    Add files via upload

---
+++ itmo.winforms.lab1.3/ChildForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace itmo.winforms.lab2
+{
+    public partial class ChildForm : itmo.winforms.lab2.MainForm
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
