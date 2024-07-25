fe4fa9cd5360c2540c3558681fe2b9dc045b41c4
Proshin-PV10 <167138897+Proshin-PV10@users.noreply.github.com>
2024-07-25 12:06:58 +0300

    Add files via upload

---
+++ itmo.winforms.lab1.1/nForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace itmo.winforms.lab1
+{
+    public partial class nForm : Form
+    {
+        private bool nclose = false;
+        public nForm()                                 
+        {
+            InitializeComponent();
+            
+        }
+       
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (checkBox1.Checked) return;
+            if (nclose) return;
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        public new void Close() 
+        { 
+            nclose = true; base.Close(); 
+        }
+    }
+}
