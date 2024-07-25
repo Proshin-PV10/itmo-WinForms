38ce1270d1e9c5744f9610f69c298134816e2ec9
Proshin-PV10 <167138897+Proshin-PV10@users.noreply.github.com>
2024-07-25 12:09:00 +0300

    Add files via upload

---
+++ itmo.winforms.lab2.2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace itmo.winforms.lab2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (peopleList.Text.Length != 0)
+            {
+                memberList.Items.Add(peopleList.Text);
+            }
+            else MessageBox.Show("Выберите элемент из списка или введите новый");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            while (memberList.CheckedIndices.Count > 0)
+                memberList.Items.RemoveAt(memberList.CheckedIndices[0]);
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            memberList.Sorted = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            peopleList.Items.Clear();
+            FileStream fStream = new FileStream("..\\..\\XMLData.xml", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            XmlDocument xmlDoc = new XmlDocument(); xmlDoc.Load(fStream);
+            for (int i = 0; i < xmlDoc.DocumentElement.ChildNodes.Count; i++)
+            {
+                peopleList.Items.Add(xmlDoc.DocumentElement.ChildNodes[i].InnerText);
+            }
+            fStream.Close();
+        }
+    }
+}
